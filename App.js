0b8d31f8c3926d69f0bbd1a83a06816d484804d4
Haider <125460927+haider36@users.noreply.github.com>
2023-05-13 01:03:17 +0500

    change the name

--- App.js
+++ App.js
@@ -5,3 +5,75 @@
 //     }
 //     console.log(star);
 // }
+
+//  if ((x === y || a === b) && c === d) {  g = h;  }  else {  e = f; 6 }
+
+// if (c === d) { if (x === y) { g = h; }  else if (a === b) { g = h; }  else {  e = f; }  } else {  e = f;  }
+
+// var Education = ["SCC","HSC","BCS","BS","BCOM","MS","MPHIL","PHD"];
+// for (var i = 0; i < Education.length; i++) {
+//     text += Education[i] + "<br>";
+//   }
+//  var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+//  var text = "";
+//  for (var i = 0; i < cars.length; i++) {
+//    text += cars[i] + "<br>";
+//  }
+ 
+
+// let myArray = [
+//     [0, 1, 2, 3], 
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+//     ];
+//     console.log(myArray);
+
+//     let MathScore = [
+//         ['John Doe', 20, 60, 'A'],
+//         ['Jane Doe', 10, 52, 'B'],
+//         ['Petr Chess', 5, 24, 'F'],
+//         ['Ling Jess', 28, 43, 'A'],
+//         ['Ben Liard', 16, 51, 'B']
+//     ];
+//     console.log(MathScore);
+
+//     var number2 ;
+//     for(var number2=1; number2<11 ; number2++ ){
+//         console.log(number2)
+//     }
+
+//    // program to generate a multiplication table
+
+// // take input from the user
+// const number3 = parseInt(prompt('Enter an integer: '));
+
+// //creating a multiplication table
+// for(let i = 1; i < 11; i++) {
+
+//     // multiply i with number
+//     const result = i * number3;
+
+//     // display the result
+//     console.log(`${number3} * ${i} = ${result}`);
+// }
+
+//  var fruits = ["apple","banana", "mango","orange", 
+//     "stawbarry"];
+// for(var i=0; i<=4;i++){
+//     console.log(fruits[i]);
+// }
+
+// var number2 ;
+//     for(var number2=1; number2<16 ; number2++ ){
+//         console.log(number2)
+//      }
+
+//      var number4 ;
+//     for(var number4=0   ; number4<16 ; number4+=2 ){
+//         console.log(number4)
+//      }
+    
+//      var greeting = ("hell0 world");
+//      alert (greeting);
+//   var howAreYou = ("iam fine and you");
+//   alert (howAreYou );
